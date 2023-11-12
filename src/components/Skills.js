bd5e3f1f7eee1f98494e5f0203c513cd2572a196
Arunabh Roy <royarunabh121@gmail.com>
2023-11-12 01:46:16 -0700

    3rd commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
+
 class Skills extends Component {
+
+  componentDidMount() {
+    // Dynamically load the Credly embed script
+    const script = document.createElement('script');
+    script.src = 'https://cdn.credly.com/assets/utilities/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }
   render() {
     if (this.props.sharedSkills && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
@@ -36,6 +45,38 @@ class Skills extends Component {
             <ul className="list-inline mx-auto skill-icon">{skills}</ul>
           </div>
         </div>
+
+        <div className="col-md-12">
+            <div className="col-md-12">
+            <h1 className="section-title" style={{ color: "black" , fontSize: "25px" }}>
+              <span >CERTIFICATIONS</span>
+            </h1>
+          </div>
+          <div className="row">
+          <div className="col-md-12 text-center">
+            <ul className="list-inline skill-icon" id="cert" >
+              <li>
+                <div data-iframe-width="400" data-iframe-height="250" data-share-badge-id="1c1e00b2-9d18-48ef-8dde-f583cf0ad2ba" data-share-badge-host="https://www.credly.com"></div>
+              </li>
+              <li>
+                <div data-iframe-width="400" data-iframe-height="250" data-share-badge-id="00c0b8bd-10d8-43d6-87e0-c1a0ce47e25d" data-share-badge-host="https://www.credly.com"></div></li>
+            </ul>
+          </div>
+          </div>
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <ul className="list-inline skill-icon" id="cert" >
+              <li>
+                <div data-iframe-width="400" data-iframe-height="250" data-share-badge-id="6ce46418-284c-4464-aef6-4ac626879b90" data-share-badge-host="https://www.credly.com"></div>
+              </li>
+              <li>
+                <div data-iframe-width="400" data-iframe-height="250" data-share-badge-id="0eec546f-b14f-4266-8435-dba52dacf240" data-share-badge-host="https://www.credly.com"></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </div>
+
       </section>
     );
   }
