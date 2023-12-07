c55da73c8d31c2756d78d36436352dbec5e7f80c
Arunabh Roy <royarunabh121@gmail.com>
2023-12-06 20:36:26 -0700

    12/6/23

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,16 +7,17 @@ function Contact() {
         email: '',
         message: ''
     });
+    const [showModal, setShowModal] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://secret-atoll-63693-f79a45670cae.herokuapp.com/send', formData);
-            console.log('Message sent:', response.data);
-            // Optionally, add logic to handle form success (like showing a success message)
+            await axios.post('https://secret-atoll-63693-f79a45670cae.herokuapp.com/send', formData);
+            setShowModal(true); // Show the modal on success
+            setFormData({ name: '', email: '', message: '' }); // Optionally, reset the form
         } catch (error) {
             console.error('Error sending message:', error);
-            // Optionally, add logic to handle form error (like showing an error message)
+            // Optionally, handle the error (e.g., show an error message)
         }
     };
 
@@ -24,54 +25,63 @@ function Contact() {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
-    return (
-        <section id="contact">
-            <h1  className="section-title" style={{ color: "black" , fontSize: "25px" }}>
-              <span className="text-black" style={{ textAlign: "center" }}>
-                CONTACT
-              </span>
-            </h1>
-            <div  className="contact-section">
-            <p className="contact-description">Feel free to reach out for collaborations or just a friendly hello!</p>
-            <form onSubmit={handleSubmit} className="contact-form">
-                <div className="form-group">
-                    <input
-                        type="text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        placeholder="Your Name"
-                        className="form-control"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        placeholder="Your Email"
-                        className="form-control"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    placeholder="Your Message"
-                    className="form-control"
-                    required
-                />
-                </div>
-                <button type="submit" className="submit-button">Send Message</button>
-            </form>
+    const Modal = ({ onClose }) => (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <p>Message sent successfully!</p>
+                <button class="submit-button" onClick={onClose}>Close</button>
             </div>
-        </section>
+        </div>
     );
 
+    return (
+        <section id="contact">
+            <h1 className="section-title" style={{ color: "black", fontSize: "25px" }}>
+                <span className="text-black" style={{ textAlign: "center" }}>
+                    CONTACT
+                </span>
+            </h1>
+            <div className="contact-section">
+                <p className="contact-description">Feel free to reach out for collaborations or just a friendly hello!</p>
+                <form onSubmit={handleSubmit} className="contact-form">
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            placeholder="Your Name"
+                            className="form-control"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="Your Email"
+                            className="form-control"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <textarea
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            placeholder="Your Message"
+                            className="form-control"
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="submit-button">Send Message</button>
+                </form>
+            </div>
+            {showModal && <Modal onClose={() => setShowModal(false)} />}
+        </section>
+    );
 }
 
 export default Contact;
